c277970ba09c76346488db93d551fb4bee25c7bc
BetsyHJ <825614102@qq.com>
2015-08-10 16:17:55 +0800

    Create ClickAnalysis.java

---
+++ src/edu/ruc/log/ClickAnalysis.java
@@ -0,0 +1,20 @@
+package edu.ruc.log;
+import edu.ruc.user.*;
+public class ClickAnalysis extends LogAnalysis {
+	
+	/**
+     * Constructor for a ClickAnalysis.
+     */
+	public ClickAnalysis(User u, long nid, long StartTime) {
+		super(u, nid, StartTime);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+     * Update user.
+     * @return updated user's feature
+     */
+	public void UpdateUser(User user) {
+		
+	}
+}
