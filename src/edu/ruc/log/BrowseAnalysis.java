a32c9efa978cdd55a959602999f88cf74de5e9fa
BetsyHJ <825614102@qq.com>
2015-08-10 16:17:07 +0800

    Create BrowseAnalysis.java

---
+++ src/edu/ruc/log/BrowseAnalysis.java
@@ -0,0 +1,20 @@
+package edu.ruc.log;
+
+import edu.ruc.user.User;
+
+public class BrowseAnalysis extends LogAnalysis {
+	/**
+     * Constructor for a BrowseAnalysis.
+     */
+	public BrowseAnalysis(User u, long nid, long startTime, long time) {
+		super(u, nid, startTime, time);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+     * Update user.
+     * @return updated user's feature
+     */
+	public void UpdateUser(User user) {
+	}
+}
