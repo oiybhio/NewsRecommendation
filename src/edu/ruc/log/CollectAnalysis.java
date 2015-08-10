6ccaf0d3c7dbfc44ae52556550dba617ca9a7a4e
BetsyHJ <825614102@qq.com>
2015-08-10 16:19:12 +0800

    Create CollectAnalysis.java

---
+++ src/edu/ruc/log/CollectAnalysis.java
@@ -0,0 +1,20 @@
+package edu.ruc.log;
+
+import edu.ruc.user.User;
+
+public class CollectAnalysis extends LogAnalysis {
+	/**
+     * Constructor for a CollectAnalysis.
+     */
+	public CollectAnalysis(User u, long nid, long StartTime) {
+		super(u, nid, StartTime);
+	} 
+	
+	/**
+     * Update user.
+     * @return updated user's feature
+     */
+	public void UpdateUser(User user) {
+		
+	}
+}
