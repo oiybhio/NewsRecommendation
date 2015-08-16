d3f6a395a58af734a92e0c85f7cde323ff626e0d
frankzwh <zwh920617@vip.qq.com>
2015-08-16 11:38:36 +0800

    update package name

---
+++ src/edu/ruc/preprocess/Tokenizer.java
@@ -0,0 +1,21 @@
+package edu.ruc.preprocess;
+
+public class Tokenizer {
+    public Tokenizer(){     
+    	
+    }
+    // 
+    public String getTokenizer(String contents){
+    	return "";
+    }
+    //
+    public String getTokenizer(String contents,String code){
+    	return "";
+    }
+    // 
+    public void putDictionary(String dicitonary){
+    	
+    }
+    
+    
+}
