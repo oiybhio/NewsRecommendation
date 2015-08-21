9e59314f8bee5fe35dd484d2e9108f41e6537445
BetsyHJ <825614102@qq.com>
2015-08-21 09:18:34 +0800

    Create Login.java

---
+++ src/edu/ruc/WebService/Login.java
@@ -0,0 +1,10 @@
+package edu.ruc.WebService;
+import net.sf.json.*;
+public class Login extends DataAnalysis{
+	private long UserID;
+	private String Password;
+	public Login(JSONObject json) {
+		UserID = json.getLong("UserID");
+		Password = json.getString("Password");
+	}
+}
