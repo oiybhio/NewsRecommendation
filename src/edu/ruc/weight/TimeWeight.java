d78077b7dd987e1d1b18da5c2f8b3dc917eca589
BetsyHJ <825614102@qq.com>
2015-08-30 11:13:47 +0800

    Create TimeWeight.java

---
+++ src/edu/ruc/weight/TimeWeight.java
@@ -0,0 +1,20 @@
+package edu.ruc.weight;
+import edu.ruc.WebService.*;
+
+public class TimeWeight {
+	/**
+	 * 考虑behaveWeight
+	 */
+	private long date;
+	private int Behave;
+	public TimeWeight(int Behave, long date){
+		this.Behave = Behave;
+		this.date = date;
+	}
+	public double getWeight() {
+		double t = 0.0;
+		double gap = (System.currentTimeMillis() - date)/1000/60/60/24;
+		t = BehaveWeight.weight[Behave] * Math.exp(-gap) ;
+		return t;
+	}
+}
