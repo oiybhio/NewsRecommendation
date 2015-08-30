3941366c5b9ef882bf1fc271a60f5099f26af7f9
BetsyHJ <825614102@qq.com>
2015-08-30 11:17:36 +0800

    Update Comment.java

--- src/edu/ruc/WebService/Comment.java
+++ src/edu/ruc/WebService/Comment.java
@@ -1,12 +1,18 @@
 package edu.ruc.WebService;
 import java.util.Date;
 
+import edu.ruc.log.ClickAnalysis;
+import edu.ruc.log.LogAnalysis;
+import edu.ruc.log.NewsAnalysis;
+import edu.ruc.news.News;
+import edu.ruc.user.User;
 import net.sf.json.*;
 public class Comment extends DataAnalysis{
 	private long UserID;
 	private long NewsID;
 	private String comment;
 	private Date date;
+	private LogAnalysis analysis;
 	
 	@SuppressWarnings("deprecation")
 	public Comment(JSONObject json) {
@@ -14,5 +20,32 @@ public class Comment extends DataAnalysis{
 		NewsID = json.getLong("NewsID");
 		comment = json.getString("comment");
 		date = new Date(json.getString("Date"));
+	}
+
+	/**
+     * Analyse user's behavior.
+     */
+	public void BehaveAnalyse(User u,News news) {
+		NewsAnalysis na = new NewsAnalysis(news,NewsID);
+		analysis = new ClickAnalysis(u,na,date.getTime());
+	}
+	/**
+     * Update user's profile.
+     */
+	public void UpdateUserProfile() {
+		analysis.UpdateUser();
+	}
+	/**
+     * Get uid.
+     */
+	public long getUid() {
+		return UserID;
+	}
+	
+	/**
+     * Get nid.
+     */
+	public long getNid() {
+		return NewsID;
 	}
 }
