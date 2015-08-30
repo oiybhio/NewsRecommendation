4d94435b112d69a437f8e0e9acfad3d4182a9141
BetsyHJ <825614102@qq.com>
2015-08-30 11:14:50 +0800

    Update Click.java

--- src/edu/ruc/WebService/Click.java
+++ src/edu/ruc/WebService/Click.java
@@ -1,16 +1,52 @@
 package edu.ruc.WebService;
 import java.util.Date;
 
+import edu.ruc.log.BrowseAnalysis;
+import edu.ruc.log.ClickAnalysis;
+import edu.ruc.log.CollectAnalysis;
+import edu.ruc.log.CommentAnalysis;
+import edu.ruc.log.LogAnalysis;
+import edu.ruc.log.NewsAnalysis;
+import edu.ruc.news.News;
+import edu.ruc.user.OnlineUsers;
+import edu.ruc.user.User;
 import net.sf.json.*;
 public class Click extends DataAnalysis{
 	private long UserID;
 	private long NewsID;
 	private Date date;
+	private LogAnalysis analysis;
 	
 	@SuppressWarnings("deprecation")
 	public Click(JSONObject json) {
 		UserID = json.getLong("UserID");
 		NewsID = json.getLong("NewsID");
 		date = new Date(json.getString("Date"));
+	}
+	/**
+     * Analyse user's behavior.
+     */
+	public void BehaveAnalyse(User u,News news) {
+		NewsAnalysis na = new NewsAnalysis(news,NewsID);
+		analysis = new ClickAnalysis(u,na,date.getTime());
+	}
+	/**
+     * Update user's profile.
+     */
+	public void UpdateUserProfile() {
+		analysis.UpdateUser();
+	}
+	/**
+     * Get uid.
+     */
+	public long getUid() {
+		return UserID;
+	}
+	
+	/**
+     * Get nid.
+     */
+	public long getNid() {
+		return NewsID;
 	}
 }
