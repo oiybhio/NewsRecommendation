49a71d728229194af3dcb05fd27cb56876596401
hneohis <oiybhio@gmail.com>
2015-09-01 23:42:04 +0800

    Update Ranker

--- src/edu/ruc/ranker/RandomMerge.java
+++ src/edu/ruc/ranker/RandomMerge.java
@@ -1,11 +1,5 @@
-/**
- * Didn't finish.
- */
-
 package edu.ruc.ranker;
 
-import edu.ruc.data.*;
-import edu.ruc.user.*;
 import edu.ruc.news.*;
 
 import java.util.*;
@@ -13,15 +7,37 @@ import java.util.*;
 public class RandomMerge {
 	private double [] p;
 	
-	public RandomMerge(double p1, double p2, double p3, double p4) {
-		p = new double [] {p1, p2, p3, p4};
+	public RandomMerge(double p1, double p2, double p3) {
+		p = new double [] {p1, p1 + p2, p1 + p2 + p3};
 	}
 	public RandomMerge() {
-		p = new double [] {0.25, 0.25, 0.25, 0.25};
+		p = new double [] {1.0/3, 2.0/3, 1.0};
 	}
 	
-	public List<News> merge(List<News> newsList1, List<News> newsList2, List<News> newsList3, List<News> newsList4) {
-		// Didn't finish.
-		return null;
+	public List<News> merge(List<List<News>> newsList, List<Long> readList) {
+		Random rand = new Random();
+		List<News> ret = new ArrayList<News>();
+		Set<Long> readSet = new HashSet<Long>();
+		for(Long nid:readList)
+			readSet.add(nid);
+		int [] i = new int [] {0, 0, 0};
+		int [] len = new int [3];
+		for(int j = 0; j < 3; j++)
+			len[j] = newsList.get(j).size();
+		while(i[0] < len[0] || i[1] < len[1] || i[2] < len[2]) {
+			double randDouble = rand.nextDouble();
+			for (int j = 0; j < 3; j++) {
+				if (randDouble <= p[j]) {
+					while (i[j] < len[j] && readSet.contains(newsList.get(j).get(i[j]).getID()))
+						i[j]++;
+					if (i[j] < len[j]) {
+						ret.add(newsList.get(j).get(i[j]));
+						readSet.add(newsList.get(j).get(i[j]).getID());
+					}
+					continue;
+				}
+			}
+		}
+		return ret;
 	}
 }
--- src/edu/ruc/ranker/Ranker.java
+++ src/edu/ruc/ranker/Ranker.java
@@ -7,79 +7,49 @@ import java.util.*;
 
 public class Ranker {
 
-	public List<News> query(ResultStore resultStore, User user, String category, List<News> candidateList) {
+	public NewsList query(ResultStore resultStore, User user, String category, List<News> candidateList, int topK) {
 		
-		int top = 10;
-		
-		List<News> newsList0, newsList1, newsList2/*, newsList3*/; 
+		List<List<News>> newsList = new ArrayList<List<News>>();
 		
 		Result result = resultStore.find(user, category, RankerType.VSM);
 		if (result == null) {
 			VSMRanker vsmRanker = new VSMRanker();
-			newsList0 = vsmRanker.query(user, candidateList);
-			resultStore.add(new Result(user, category, RankerType.VSM, newsList0));
+			newsList.add(vsmRanker.query(user, candidateList));
+			resultStore.add(new Result(user, category, RankerType.VSM, newsList.get(0)));
 		} else {
-			newsList0 = result.getNewsList();
+			newsList.add(result.getNewsList());
 		}
-		
-		System.out.println("UserId:" + user.getUid() + " Category:" + category + " Ranker:VSM");
-		int i = 0;
-		for(News news:newsList0) {
-			System.out.println(news.getTitle());
-			i++;
-			if (i >= top) break;
-		}
-		System.out.println();
 		
 		result = resultStore.find(user, category, RankerType.POPULARITY);
 		if (result == null) {
 			PopularityRanker popularityRanker = new PopularityRanker();
-			newsList1 = popularityRanker.query(user, candidateList);
-			resultStore.add(new Result(user, category, RankerType.POPULARITY, newsList1));
+			newsList.add(popularityRanker.query(user, candidateList));
+			resultStore.add(new Result(user, category, RankerType.POPULARITY, newsList.get(1)));
 		} else {
-			newsList1 = result.getNewsList();
+			newsList.add(result.getNewsList());
 		}
-
-		System.out.println("UserId:" + user.getUid() + " Category:" + category + " Ranker:Hotness");
-		i = 0;
-		for(News news:newsList1) {
-			System.out.println(news.getTitle());
-			i++;
-			if (i >= top) break;
-		}
-		System.out.println();		
 		
 		result = resultStore.find(user, category, RankerType.TIME);
 		if (result == null) {
 			TimeRanker timeRanker = new TimeRanker();
-			newsList2 = timeRanker.query(user, candidateList);
-			resultStore.add(new Result(user, category, RankerType.TIME, newsList2));
+			newsList.add(timeRanker.query(user, candidateList));
+			resultStore.add(new Result(user, category, RankerType.TIME, newsList.get(2)));
 		} else {
-			newsList2 = result.getNewsList();
+			newsList.add(result.getNewsList());
 		}
-
-		System.out.println("UserId:" + user.getUid() + " Category:" + category + " Ranker:Time");
-		i = 0;
-		for(News news:newsList2) {
-			System.out.println(news.getTitle());
-			i++;
-			if (i >= top) break;
+		
+		RandomMerge randomMerge = new RandomMerge();
+		List<News> newsListMerged = randomMerge.merge(newsList, user.getReaded());
+		
+		NewsList ret = new NewsList();
+		if (newsListMerged.size() < topK)
+			topK = newsListMerged.size();
+		for(int i = 0; i < topK; i++) {
+			ret.addNews(newsListMerged.get(i));
+			//System.out.println(newsListMerged.get(i).title);
 		}
-		System.out.println();		
-	
-		/*result = resultStore.find(user, category, RankerType.LEARNING);
-		if (result == null) {
-			LearningRanker learningRanker = new LearningRanker();
-			newsList3 = learningRanker.query(user, candidateList);
-			resultStore.add(new Result(user, category, RankerType.LEARNING, newsList3));
-		} else {
-			newsList3 = result.getNewsList();
-		}*/
 		
-		//RandomMerge randomMerge = new RandomMerge(0.25, 0.25, 0.25, 0.25);
-		//newsList = randomMerge.merge(newsList0, newsList1, newsList2, newsList3);
-		
-		return null; // Didn't finish.
+		return ret;
 	}
 	
 }
