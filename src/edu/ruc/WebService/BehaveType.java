13c89d30d3d8a114145984414a9d7f7779b7fa5c
BetsyHJ <825614102@qq.com>
2015-09-02 16:16:34 +0800

    Update BehaveType.java

--- src/edu/ruc/WebService/BehaveType.java
+++ src/edu/ruc/WebService/BehaveType.java
@@ -1,5 +1,5 @@
 package edu.ruc.WebService;
 
 public enum BehaveType {
-	Register,Login,Modify,Recommend,Click,Refresh,Query,Comment,Log,Collect,Browse;
+	Click,Comment,Log,Collect,Browse,Register,Login,Modify,Recommend,Refresh,Query;
 }
