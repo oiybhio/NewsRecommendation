f96e84ee0d909f086ff4cbb8a4097cd35fb2f85f
BetsyHJ <825614102@qq.com>
2015-09-02 21:16:43 +0800

    Update DataAnalysis.java

--- src/edu/ruc/WebService/DataAnalysis.java
+++ src/edu/ruc/WebService/DataAnalysis.java
@@ -1,5 +1,31 @@
 package edu.ruc.WebService;
 
+import edu.ruc.log.ClickAnalysis;
+import edu.ruc.log.NewsAnalysis;
+import edu.ruc.news.News;
+import edu.ruc.user.User;
+
 public class DataAnalysis {
-	private long UserID;
+	public long UserID;
+	public long NewsID;
+	public void BehaveAnalyse(User u,News news) {
+	}
+	/**
+     * Update user's profile.
+     */
+	public void UpdateUserProfile() {
+	}
+	/**
+     * Get uid.
+     */
+	public long getUid() {
+		return UserID;
+	}
+	
+	/**
+     * Get nid.
+     */
+	public long getNid() {
+		return NewsID;
+	}
 }
