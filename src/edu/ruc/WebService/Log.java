4e19e0e80d6f224085cd5bafd9a7f5cd1cb2cb72
BetsyHJ <825614102@qq.com>
2015-09-08 17:42:59 +0800

    Update Log.java

--- src/edu/ruc/WebService/Log.java
+++ src/edu/ruc/WebService/Log.java
@@ -1,22 +1,44 @@
 package edu.ruc.WebService;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 
 import net.sf.json.*;
 public class Log extends DataAnalysis{
-	private long UserID;
-	private long NewsID;
-	private Date StartTime;
-	private long Time;
-	private int Action;
-	private String Info;
+	private String js;
+	private JSONObject json;
 	
 	@SuppressWarnings("deprecation")
-	public Log(JSONObject json) {
-		UserID = json.getLong("UserID");
-		NewsID = json.getLong("NewsID");
-		StartTime = new Date(json.getString("StartTime"));
-		Time = json.getLong("Time");
-		Action = json.getInt("Action");
-		Info = json.getString("Info");
+	public Log(JSONObject J) {
+		json = J;
+		js = J.toString();
+		UserID = J.getLong("UserID");
+		NewsID = J.getLong("NewsID");
+	}
+	public void Store(Connection con) throws SQLException {
+		String strsql = "insert into log(logID, uid, json, flag)" + " values(?,?,?,?)";
+		long uid = json.getLong("UserID");
+		String logID = ""+System.currentTimeMillis()+""+uid;
+
+		PreparedStatement pstmt = con.prepareStatement(strsql);
+	    pstmt.setString(1, logID);
+	    pstmt.setLong(2, uid);
+	    pstmt.setString(3, js);
+	    pstmt.setInt(4, 0);
+	    pstmt.execute();
+	}
+	/**
+     * Get uid.
+     */
+	public long getUid() {
+		return UserID;
+	}
+	
+	/**
+     * Get nid.
+     */
+	public long getNid() {
+		return NewsID;
 	}
 }
